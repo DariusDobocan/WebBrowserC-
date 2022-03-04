b10719fe9eedd3c6f5aa2f8e6378a07473ebd0fb
DariusDobocan <91589966+DariusDobocan@users.noreply.github.com>
2022-03-04 16:55:13 +0200

    Add files via upload

---
+++ PaginaWeb/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace PaginaWeb
+{
+    public partial class Form1 : Form
+    {
+        
+        private List<string> lstBlock1;
+        private SQLiteConnection conn = null;
+        private Thread myThread;
+        private BooleanSwitch sw;
+
+    public Form1()
+        {
+            
+            FileStream file = new FileStream("testLog.txt", FileMode.OpenOrCreate);
+            TextWriterTraceListener traceListener = new TextWriterTraceListener(file);
+            Trace.Listeners.Add(traceListener);
+            Trace.AutoFlush = true;
+            sw = new BooleanSwitch("Switch1", "TRACE SWITCH");
+            InitializeComponent();
+        }
+        private void run()
+        {
+            Console.WriteLine("The thread has started");
+
+            
+                Trace.WriteLine("");
+                
+
+        }
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            //webBrowser1.GoHome();
+            webBrowser1.Navigate("www.google.com");
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            
+            webBrowser1.Navigate(this.toolStripTextBox1.Text);
+            Trace.WriteLine(this.toolStripTextBox1.Text);
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            string url = e.Url.ToString();
+            lstBlock1 = SQLiteHandler.GetAllKeywords(this.conn);
+            
+            var res = Task.Run(() => CheckBlock(url));
+
+            res.Wait();
+
+            if (res.Result)
+            {
+                e.Cancel = true;
+                MessageBox.Show("Url blocked");
+            }
+        }
+            private async Task<bool> CheckBlock(string url)
+            {
+                bool res = await Task.Run(async () =>
+               {
+                   return (from x in lstBlock1 where url.Contains(x) select x).Count() > 0;
+               });
+            return res;
+            }
+
+        private void connectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           this.conn = SQLiteHandler.ConnectToDb();
+            Console.WriteLine("Connected to database");
+
+        }
+
+        private void disconnectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SQLiteHandler.DisconnectFromDB(this.conn);
+            Console.WriteLine("Disconnected from database");
+        }
+
+        private void addKeywordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 kwdForm = new Form2(); 
+            if(kwdForm.ShowDialog() == DialogResult.OK)
+            {
+                string kwd = kwdForm.getImputText();
+                SQLiteHandler.InsertKeyword(this.conn, kwd);
+                MessageBox.Show(string.Format("S-a introdus {0}", kwd));
+            }
+        }
+
+        private void toolStripTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                webBrowser1.Navigate(this.toolStripTextBox1.Text);
+                Trace.WriteLine(this.toolStripTextBox1.Text);
+            }
+        }
+
+        private void cuvCheie1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        bool x = true;
+        private void cuvCheie1_DropDown(object sender, EventArgs e)
+        {
+            lstBlock1 = SQLiteHandler.GetAllKeywords(this.conn);
+            if (x == true)
+            {
+                foreach (string s in lstBlock1)
+                {
+                    cuvCheie1.Items.Add(s);
+                }
+                x= false;
+            }
+        }
+    }
+}
---
+++ PaginaWeb/Form2.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PaginaWeb
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnOk = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnOk
+            // 
+            this.BtnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.BtnOk.Location = new System.Drawing.Point(12, 88);
+            this.BtnOk.Name = "BtnOk";
+            this.BtnOk.Size = new System.Drawing.Size(75, 23);
+            this.BtnOk.TabIndex = 0;
+            this.BtnOk.Text = "OK";
+            this.BtnOk.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnCancel.Location = new System.Drawing.Point(147, 87);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancel.TabIndex = 1;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Keyword:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 134);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnOk);
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnOk;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
